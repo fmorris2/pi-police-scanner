6c0b088aa9cbece62bf4959c338561aaf23de056
fmorris2 <fred11212@hotmail.com>
2021-04-04 00:52:49 +0000

    Parse scanner is now in while loop

--- play_scanner.sh
+++ play_scanner.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 while true; do
-	mplayer -nocache http://broadcastify.cdnstream1.com/31719
+	mplayer -nocache http://broadcastify.cdnstream1.com/31719 | sudo /home/ubuntu/pi-police-scanner/parse_scanner.py
 done
